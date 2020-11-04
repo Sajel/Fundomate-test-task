1d4ead7a0c5e326dc568372efb0a54900ff021bd
Andrii Khanevych <a.khanevych@spd-ukraine.com>
2020-11-04 22:19:34 +0200

    Add three level hierarchy test for task 1

--- src/test/java/fundomate/CompensationCalculatorTest.java
+++ src/test/java/fundomate/CompensationCalculatorTest.java
@@ -3,11 +3,11 @@ package fundomate;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Optional;
 
 import static java.lang.String.format;
+import static java.util.Arrays.asList;
 import static java.util.Collections.singleton;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -23,17 +23,63 @@ class CompensationCalculatorTest {
         Partner subPartner1 = createPartner("Sub-partner1", 0.3);
         Partner subPartner2 = createPartner("Sub-partner2", 0.3);
         Partner subPartner3 = createPartner("Sub-partner3", 0.3);
+        topPartner.setSubPartners(asList(subPartner1, subPartner2, subPartner3));
 
-        topPartner.setSubPartners(Arrays.asList(subPartner1, subPartner2, subPartner3));
 
-
-        Collection<CompensationCalculationResult> calculationResults = compensationCalculator.calculate(singleton(topPartner), BigDecimal.valueOf(1000));
+        BigDecimal fullCompensation = BigDecimal.valueOf(1000);
+        Collection<CompensationCalculationResult> calculationResults = compensationCalculator.calculate(singleton(topPartner), fullCompensation);
 
         assertAll(
                 () -> assertPartnerCompensation(calculationResults, "Top-partner", 50),
                 () -> assertPartnerCompensation(calculationResults, "Sub-partner1", 150),
                 () -> assertPartnerCompensation(calculationResults, "Sub-partner2", 150),
                 () -> assertPartnerCompensation(calculationResults, "Sub-partner3", 150)
+        );
+    }
+
+    /**
+     * Full compensation - 1000$
+     * TopPartner 1 - 40% - 400$ - 20$ in the result
+     *      Partner 1.1 - 50% - 200$ - 20$ in the result
+     *          Partner 1.1.1 - 40% - 80$
+     *          Partner 1.1.2 - 50% - 100$
+     *      Partner 1.2 - 45% - 180$
+     * TopPartner 2 - 40% - 400$ - 40$ in the result
+     *      Partner 2.1 - 30% - 120$
+     *      Partner 2.2 - 30% - 120$
+     *      Partner 2.3 - 30% - 120$
+     */
+    @Test
+    void three_level_hierarchy_example() {
+        Partner topPartner1 = createPartner("Top-partner1", 0.4);
+
+        Partner partner1_1 = createPartner("Partner1.1", 0.5);
+        Partner partner1_1_1 = createPartner("Partner1.1.1", 0.4);
+        Partner partner1_1_2 = createPartner("Partner1.1.2", 0.5);
+        partner1_1.setSubPartners(asList(partner1_1_1, partner1_1_2));
+        Partner partner1_2 = createPartner("Partner1.2", 0.45);
+        topPartner1.setSubPartners(asList(partner1_1, partner1_2));
+
+        Partner topPartner2 = createPartner("Top-partner2", 0.4);
+        Partner partner2_1 = createPartner("Partner2.1", 0.3);
+        Partner partner2_2 = createPartner("Partner2.2", 0.3);
+        Partner partner2_3 = createPartner("Partner2.3", 0.3);
+        topPartner2.setSubPartners(asList(partner2_1, partner2_2, partner2_3));
+
+
+        BigDecimal fullCompensation = BigDecimal.valueOf(1000);
+        Collection<CompensationCalculationResult> calculationResults = compensationCalculator.calculate(asList(topPartner1, topPartner2), fullCompensation);
+
+        assertAll(
+                () -> assertPartnerCompensation(calculationResults, "Top-partner1", 20),
+                () -> assertPartnerCompensation(calculationResults, "Partner1.1", 20),
+                () -> assertPartnerCompensation(calculationResults, "Partner1.1.1", 80),
+                () -> assertPartnerCompensation(calculationResults, "Partner1.1.2", 100),
+                () -> assertPartnerCompensation(calculationResults, "Partner1.2", 180),
+                () -> assertPartnerCompensation(calculationResults, "Top-partner2", 40),
+                () -> assertPartnerCompensation(calculationResults, "Partner2.1", 120),
+                () -> assertPartnerCompensation(calculationResults, "Partner2.2", 120),
+                () -> assertPartnerCompensation(calculationResults, "Partner2.3", 120)
         );
     }
 
@@ -48,7 +94,10 @@ class CompensationCalculatorTest {
         BigDecimal expectedCompensationBigDecimal = BigDecimal.valueOf(expectedCompensation.doubleValue());
         BigDecimal actualCompensation = partnerResult.get().getCompensation();
         boolean correctCompensation = expectedCompensationBigDecimal.compareTo(actualCompensation) == 0;
-        assertTrue(correctCompensation, format("Wrong calculated compensation, expected - [%s], actual - [%s]", expectedCompensationBigDecimal, actualCompensation));
+        assertTrue(correctCompensation, format("Wrong calculated compensation for [%s], expected - [%s], actual - [%s]",
+                partnerResult.get().getPartnerName(),
+                expectedCompensationBigDecimal,
+                actualCompensation));
     }
 
     private Partner createPartner(String name, double fee) {
